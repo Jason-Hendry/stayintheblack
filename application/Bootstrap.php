87c1bd301794b0c019070141102accb7b35c4c67
Jason Hendry <jason+github.com@rain.com.au>
2012-05-24 23:40:22 +1000

    Initialize Doctrine

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,10 +3,44 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
-  public function init() {
+
+  public function initTZ() {
     date_default_timezone_set('Australia/Melbourne');
   }
 
+  public function _initDoctrine() {
+    $lib = realpath(dirname(__FILE__).'/../library');
+    require "Doctrine/Common/ClassLoader.php";
+    $doctrineCommonLoader = new \Doctrine\Common\ClassLoader("Doctrine\Common",$lib);
+    $doctrineCommonLoader->register();
+    $dbalLoader = new \Doctrine\Common\ClassLoader("Doctrine\DBAL",$lib);
+    $dbalLoader->register();
+    $ormLoader = new \Doctrine\Common\ClassLoader("Doctrine\ORM",$lib);
+    $ormLoader->register();
+
+    // Fetch Application Config Options
+    $conf = $this->getApplication()->getOptions();
+ 
+    //TODO Setup ApcCache on production
+    $cache = new \Doctrine\Common\Cache\ArrayCache();
+    $config = new \Doctrine\ORM\Configuration();
+    $config->setMetadataCacheImpl($cache);
+    $driverImpl = $config->newDefaultAnnotationDriver($conf['doctrine']['entities']);
+    $config->setMetadataDriverImpl($driverImpl);
+    $config->setQueryCacheImpl($cache);
+    $config->setProxyDir($conf['doctrine']['proxyPath']);
+    $config->setProxyNamespace($conf['doctrine']['proxyNamespace']);
+    //TODO disable on production
+    $config->setAutoGenerateProxyClasses(true);
+
+    
+    $em = \Doctrine\ORM\EntityManager::create($conf['doctrine']['connection'],$config);
+        
+    //Zend_Debug::dump($em->Find('Application\Payment',1));
+
+
+    return $em;
+  }
   public function _initDocType() {
     $this->bootstrap('view');
     $view = $this->getResource('view');
